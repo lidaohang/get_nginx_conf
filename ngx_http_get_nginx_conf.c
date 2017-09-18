e913a8cfb31dc7bcaceefacd483fd35d89a7aa03
Lucien <357732053@126.com>
2017-09-18 14:20:29 +0800

    Update ngx_http_get_nginx_conf.c

--- ngx_http_get_nginx_conf.c
+++ ngx_http_get_nginx_conf.c
@@ -1,50 +1,84 @@
-static ngx_str_t                    HTTP_KEY = "http"
-static ngx_str_t                    SERVERS_KEY = "servers"
-static ngx_str_t                    SERVER_NAME_KEY = "server_name";
-static ngx_str_t                    SERVER_PORT_KEY = "server_port";
-static ngx_str_t                    LOCATIONS_KEY = "locations";
-static ngx_str_t                    LOCATION_KEY = "location";
-
-
-
 static void
-ngx_http_didi_conf_get_static_locations(yajl_gen &g, ngx_http_location_tree_node_t *node)
+ngx_http_lua_conf_get_static_locations(yajl_gen g, ngx_http_location_tree_node_t *node)
 {
-    
     if ( node == NULL ) {
         return;
     }
 
-    yajl_gen_string(g, (unsigned char *) LOCATION_KEY.data, LOCATION_KEY.len);
     yajl_gen_string(g, (unsigned char *)node->name, node->len);
 
-
     if ( node->left ) {
-        return ngx_http_get_test_locations(r,node->left);
+        return ngx_http_lua_conf_get_static_locations(g, node->left);
     }
 
     if ( node->right ) {
-        return ngx_http_get_test_locations(r,node->right);
+        return ngx_http_lua_conf_get_static_locations(g, node->right);
     }
 
 }
 
-
 static void
-ngx_http_didi_conf_get_regex_locations(yajl_gen &g, ngx_http_core_loc_conf_t **clcfp)
+ngx_http_lua_conf_get_locations(yajl_gen g, ngx_http_core_loc_conf_t *clcf)
 {
+    ngx_http_core_loc_conf_t                    **clcfp;
 
-    if ( clcfp ) {
+    if ( clcf ==  NULL ) {
         return;
     }
 
-    for (clcfp = clcf->regex_locations; *clcfp; clcfp++) {
+    yajl_gen_string(g, (unsigned char *) LOCATIONS_KEY, ngx_strlen(LOCATIONS_KEY));
 
-        yajl_gen_string(g, (unsigned char *) LOCATION_KEY.data, LOCATION_KEY.len);
-        yajl_gen_string(g, (unsigned char *)(*clcfp)->name.data (*clcfp)->name.len);                
+    yajl_gen_array_open(g);
+
+    ngx_http_lua_conf_get_static_locations(g, clcf->static_locations);
+
+    if ( clcf->regex_locations == NULL ) {
+        goto array_close;
     }
 
+    for (clcfp = clcf->regex_locations; *clcfp; clcfp++) {
+        yajl_gen_string(g, (unsigned char *)(*clcfp)->name.data , (*clcfp)->name.len);
+    }
+
+array_close:
+    yajl_gen_array_close(g);
+
 }
+
+static void
+ngx_http_lua_conf_get_servers(yajl_gen g, ngx_http_core_srv_conf_t *server,
+        ngx_http_conf_addr_t *addr)
+{
+    size_t                                      n;
+    ngx_http_server_name_t                      *name;
+    ngx_http_core_loc_conf_t                    *clcf;
+
+
+    yajl_gen_map_open(g);
+    name = server->server_names.elts;
+
+    //server_name
+    yajl_gen_string(g, (unsigned char *)SERVER_NAME_KEY, ngx_strlen(SERVER_NAME_KEY));
+    yajl_gen_array_open(g);
+
+    for (n = 0; n < server->server_names.nelts; n++) {
+        yajl_gen_string(g, (unsigned char *)name[n].name.data, name[n].name.len);
+    }
+
+    yajl_gen_array_close(g);
+
+    //server_port
+    yajl_gen_string(g, (unsigned char *) SERVER_PORT_KEY, strlen(SERVER_PORT_KEY));
+    yajl_gen_string(g, (unsigned char *) addr->opt.addr, ngx_strlen(addr->opt.addr));
+
+    clcf = server->ctx->loc_conf[ngx_http_core_module.ctx_index];
+
+    ngx_http_lua_conf_get_locations(g, clcf);
+
+    yajl_gen_map_close(g);
+}
+
+
 
 /*
 {
@@ -54,12 +88,8 @@ ngx_http_didi_conf_get_regex_locations(yajl_gen &g, ngx_http_core_loc_conf_t **c
                 "server_name":"www.didichuxing.com",
                 "port":80,
                 "locations":[
-                    {
-                        "location":"/index"
-                    },
-                    {
+                        "location":"/index",
                         "location":"/home"
-                    }
                 ]
             }
         ]
@@ -67,29 +97,24 @@ ngx_http_didi_conf_get_regex_locations(yajl_gen &g, ngx_http_core_loc_conf_t **c
 }
 */
 
-
-static ngx_int_t
-ngx_http_didi_conf_get(lua_State *L)
+static int
+ngx_http_lua_ngx_get_conf(lua_State *L)
 {
-    size_t                                      s, i, j, n;
+    size_t                                      s, i, j;
     size_t                                      len;
     ngx_http_conf_addr_t                        *addr;
     ngx_http_conf_port_t                        *port;
     const unsigned char                         *buf;
-    ngx_http_request_t                          *r;
     ngx_http_core_main_conf_t                   *cmcf;
-    ngx_http_core_loc_conf_t                    *clcf;
     ngx_http_core_srv_conf_t                    **server;
-    ngx_http_server_name_t                      *name;
     yajl_gen                                    g;
-    yajl_val                                    node;
     yajl_gen_status                             status;
-
 
 
     g = yajl_gen_alloc(NULL);
     if ( g == NULL ) {
-        return NGX_ERROR;
+        lua_pushnil(L);
+        return 1;
     }
 
     yajl_gen_config(g, yajl_gen_beautify, 0);
@@ -97,19 +122,17 @@ ngx_http_didi_conf_get(lua_State *L)
     yajl_gen_map_open(g);
 
     //http
-    yajl_gen_string(g, (unsigned char *)HTTP_KEY.data, HTTP_KEY.len);
-
+    yajl_gen_string(g, (unsigned char *)HTTP_KEY, strlen(HTTP_KEY));
 
     yajl_gen_map_open(g);
 
     //servers
-    yajl_gen_string(g, (unsigned char *)SERVERS_KEY.data, SERVERS_KEY.len);
-
+    yajl_gen_string(g, (unsigned char *)SERVERS_KEY, ngx_strlen(SERVERS_KEY));
 
     yajl_gen_array_open(g);
 
-
-    cmcf = ngx_http_get_module_main_conf(r, ngx_http_core_module);
+    cmcf = ngx_http_cycle_get_module_main_conf(ngx_cycle, ngx_http_core_module);
+    //cmcf = ngx_http_get_module_main_conf(r, ngx_http_core_module);
 
     port = cmcf->ports->elts;
     for (s = 0; s < cmcf->ports->nelts; s++) {
@@ -117,41 +140,12 @@ ngx_http_didi_conf_get(lua_State *L)
         addr = port[s].addrs.elts;
         for (i = 0; i < port[s].addrs.nelts; i++) {
 
-            
-
             server = addr[i].servers.elts;
             for (j = 0; j < addr[i].servers.nelts; j++) {
 
-                yajl_gen_map_open(g);
-
-                name = server[j]->server_names.elts;
-
-                for (n = 0; n < server[j]->server_names.nelts; n++) {
-
-                    //server_name
-                    yajl_gen_string(g, (unsigned char *)SERVER_NAME_KEY.data, SERVER_NAME_KEY.len);
-                    yajl_gen_string(g, (unsigned char *)name[n].name.data, name[n].name.len);
-
-                    //server_port
-                    yajl_gen_string(g, (unsigned char *) SERVER_PORT_KEY.data, SERVER_PORT_KEY.len);
-                    yajl_gen_string(g, (unsigned char *) addr[i].opt.addr, ngx_len(addr[i].opt.addr));
-                }
-
-                clcf = server[j]->ctx->loc_conf[ngx_http_core_module.ctx_index];
-
-                yajl_gen_string(g, (char *) LOCATIONS_KEY.data, LOCATIONS_KEY.len);
-                
-                yajl_gen_array_open(g);
-
-                ngx_http_didi_conf_get_static_locations(g, clcf->static_locations);
-
-                ngx_http_didi_conf_get_regex_locations(g, clcf->regex_locations);
-
-                yajl_gen_array_close(g);
-
-                yajl_gen_map_close(g);
+                ngx_http_lua_conf_get_servers(g, server[j], &addr[i]);
             }
-            
+
         }
     }
 
@@ -164,7 +158,7 @@ ngx_http_didi_conf_get(lua_State *L)
     status = yajl_gen_get_buf(g, &buf, &len);
     if(status != yajl_gen_status_ok) {
         yajl_gen_free(g);
-        lua_pushlstring(L);
+        lua_pushnil(L);
 
         return 1;
     }
